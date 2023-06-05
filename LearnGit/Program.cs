0e0fa856ce2b6d3b4dcc130b351875136eb98f4c
Shaowei Chen <schen@premier-us.net>
2023-06-05 16:15:10 -0500

    feature-002 2023.06.05 16.14

--- LearnGit/Program.cs
+++ LearnGit/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I start to work on feature-001 2023.06.05 14:47!");
 
 Console.WriteLine("I start to work on feature-002 2023.06.05 15:36!");
+
+Console.WriteLine("I continute to work on feature-002 2023.06.05 16.14!");
