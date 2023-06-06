a2b871ffdc0b04960ef4b460561d4683392be191
happyat1 <115040343+happyat1@users.noreply.github.com>
2023-06-06 16:04:16 -0500

    origin/feature-001 2023.06.06 15:19

--- LearnGit/Program.cs
+++ LearnGit/Program.cs
@@ -8,4 +8,8 @@ Console.WriteLine("I start to work on feature-002 2023.06.05 15:36!");
 
 Console.WriteLine("I continute to work on feature-002 2023.06.05 16.14!");
 
+Console.WriteLine("I continue to work on feature-001 2023.06.06 15:17!");
+
 Console.WriteLine("I continute to work on feature-002 2023.06.05 16.34!");
+
+Console.WriteLine("I work on origin/feature-001 2023.06.06 15:19!");
