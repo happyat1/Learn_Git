e35fbc50dcbf060a3098bc068d17abbaab9fd2a7
Shaowei Chen <schen@premier-us.net>
2023-06-08 14:25:17 -0500

    feature-001 feature-001 2023.06.08 14.12

---
+++ LearnGit/Feature-001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGit
+{
+    public class Feature_001
+    {
+        public static void PrintLog()
+        
+        {
+            Console.WriteLine("I start to work on new feature-001 2023.06.08 14.12!");
+
+
+            return;
+        }
+
+
+    }
+}
--- LearnGit/Program.cs
+++ LearnGit/Program.cs
@@ -1,15 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LearnGit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace AlgorithmPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// See https://aka.ms/new-console-template for more information
+            Feature_001.PrintLog();
 
-Console.WriteLine("I start to work on feature-001 2023.06.05 14:47!");
-
-Console.WriteLine("I start to work on feature-002 2023.06.05 15:36!");
-
-Console.WriteLine("I continute to work on feature-002 2023.06.05 16.14!");
-
-Console.WriteLine("I start to work on origin/develop 2023.06.05 06.33!");
-
-Console.WriteLine("I continute to work on feature-002 2023.06.05 16.34!");
-
-Console.WriteLine("I start to work on feature-003 2023.06.06 16:59!");
+        }
+    }
+}
